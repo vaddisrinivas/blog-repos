7f714334f091ece9f45232bd2b972968b9a371e5
Srinivas Vaddi <vaddisrinivas170497@gmail.com>
2023-10-12 12:05:00 -0400

    updating OAS

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,9 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from sqlalchemy.exc import IntegrityError
 import configparser
 import logging
 import os
 from flask_sqlalchemy import SQLAlchemy
 from pprint import pprint
+from models import File, Project, Team, db
+# from flask_restplus import Api, Resource
+
 config_file = f"config_{os.getenv('env',default='dev')}.cfg"
 config = configparser.ConfigParser()
 config.read(config_file)
@@ -13,18 +17,64 @@ logging.basicConfig(filename=config.get("app","log_file"), level=logging_levels[
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = config.get("app","db_uri")
-db = SQLAlchemy(app)
+db.init_app(app)
 session = db.session
-class File(db.Model):
-    __tablename__ = 'files'
-    id = db.Column(db.String(36), primary_key=True)
-    content = db.Column(db.String(255), nullable=False)
 
+@app.route('/teams', methods=['POST'])
+def create_team():
+    data = request.json
+    try:
+        team = Team(**data)
+        db.session.add(team)
+        db.session.commit()
+        return jsonify(team.serialize()), 201
+    except IntegrityError:
+        db.session.rollback()
+        return "Team with the same name or domains already exists.", 409
 
-@app.route('/api/upload', methods=['POST',"GET"])
-def get_projects():
-    if request.method == 'POST':
-        # check if the post request has the file part
+# List all teams
+@app.route('/teams', methods=['GET'])
+def list_teams():
+    teams = Team.query.all()
+    return jsonify([team.serialize() for team in teams])
+
+# Get a specific team by ID
+@app.route('/teams/<int:team_id>', methods=['GET'])
+def get_team(team_id):
+    team = Team.query.get(team_id)
+    if team is not None:
+        return jsonify(team.serialize())
+    else:
+        return "Team not found", 404
+
+# Update a team by ID
+@app.route('/teams/<int:team_id>', methods=['PUT'])
+def update_team(team_id):
+    team = Team.query.get(team_id)
+    if team is not None:
+        data = request.json
+        for key, value in data.items():
+            setattr(team, key, value)
+        db.session.commit()
+        return jsonify(team.serialize())
+    else:
+        return "Team not found", 404
+
+# Delete a team by ID
+@app.route('/teams/<int:team_id>', methods=['DELETE'])
+def delete_team(team_id):
+    team = Team.query.get(team_id)
+    if team is not None:
+        db.session.delete(team)
+        db.session.commit()
+        return "", 204
+    else:
+        return "Team not found", 404
+
+# Create a new File
+@app.route('/files', methods=['POST'])
+def create_file():
+    try:
         if 'file' not in request.files:
             return "No file part"
         file = request.files['file']
@@ -36,22 +86,97 @@ def get_projects():
             f = File(id=file.filename,content=file.read())
             session.add(f)
             db.session.commit()
-            return f.id
-    elif request.method == 'GET':
-        f = File.query.filter_by(id=request.args.get('id')).first()
-        return f.content
-        # return "done"
-    
+            return f.id, 201
+    except IntegrityError:
+        db.session.rollback()
+        return "File with the same name already exists in the project.", 409
 
-@app.route('/api/differences', methods=['GET', "POST"])
-def get_project_files():
-    if request.method == 'GET':
-        return "Hello World!"
-    elif request.method == 'POST':
-        return "Hello World!"
-    pprint(request.__dict__)
-    return "Hello World!"
+# List all files
+@app.route('/files', methods=['GET'])
+def list_files():
+    files = File.query.all()
+    return jsonify([file.serialize() for file in files])
 
+# Get a specific file by ID
+@app.route('/files/<int:file_id>', methods=['GET'])
+def get_file(file_id):
+    file = File.query.get(file_id)
+    if file is not None:
+        return jsonify(file.serialize())
+    else:
+        return "File not found", 404
+
+# Update a file by ID
+@app.route('/files/<int:file_id>', methods=['PUT'])
+def update_file(file_id):
+    file = File.query.get(file_id)
+    if file is not None:
+        data = request.json
+        for key, value in data.items():
+            setattr(file, key, value)
+        db.session.commit()
+        return jsonify(file.serialize())
+    else:
+        return "File not found", 404
+
+# Delete a file by ID
+@app.route('/files/<int:file_id>', methods=['DELETE'])
+def delete_file(file_id):
+    file = File.query.get(file_id)
+    if file is not None:
+        db.session.delete(file)
+        db.session.commit()
+        return "", 204
+    else:
+        return "File not found", 404
+
+# Create a new Project
+@app.route('/projects', methods=['POST'])
+def create_project():
+    data = request.json
+    project = Project(**data)
+    db.session.add(project)
+    db.session.commit()
+    return jsonify(project.serialize()), 201
+
+# List all projects
+@app.route('/projects', methods=['GET'])
+def list_projects():
+    projects = Project.query.all()
+    return jsonify([project.serialize() for project in projects])
+
+# Get a specific project by ID
+@app.route('/projects/<int:project_id>', methods=['GET'])
+def get_project(project_id):
+    project = Project.query.get(project_id)
+    if project is not None:
+        return jsonify(project.serialize())
+    else:
+        return "Project not found", 404
+
+# Update a project by ID
+@app.route('/projects/<int:project_id>', methods=['PUT'])
+def update_project(project_id):
+    project = Project.query.get(project_id)
+    if project is not None:
+        data = request.json
+        for key, value in data.items():
+            setattr(project, key, value)
+        db.session.commit()
+        return jsonify(project.serialize())
+    else:
+        return "Project not found", 404
+
+# Delete a project by ID
+@app.route('/projects/<int:project_id>', methods=['DELETE'])
+def delete_project(project_id):
+    project = Project.query.get(project_id)
+    if project is not None:
+        db.session.delete(project)
+        db.session.commit()
+        return "", 204
+    else:
+        return "Project not found", 404
 
 if __name__ == '__main__':
     with app.app_context():
