0ebbfc4c54ec1d16c0058f8b0334ec9e627c0775
Srinivas Vaddi <vaddisrinivas170497@gmail.com>
2023-10-11 23:19:52 -0400

    working file upload

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -16,6 +16,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = config.get("app","db_uri")
 db = SQLAlchemy(app)
 session = db.session
 class File(db.Model):
+    __tablename__ = 'files'
     id = db.Column(db.String(36), primary_key=True)
     content = db.Column(db.String(255), nullable=False)
 
@@ -34,10 +35,12 @@ def get_projects():
         if file:
             f = File(id=file.filename,content=file.read())
             session.add(f)
+            db.session.commit()
             return f.id
     elif request.method == 'GET':
-
-        return session.query(File).get(request.args.get('id')).content
+        f = File.query.filter_by(id=request.args.get('id')).first()
+        return f.content
+        # return "done"
     
 
 @app.route('/api/differences', methods=['GET', "POST"])
@@ -51,4 +54,6 @@ def get_project_files():
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(host=config.get("app","host"),port=config.getint("app","port"),debug=config.getboolean("app","debug"))
